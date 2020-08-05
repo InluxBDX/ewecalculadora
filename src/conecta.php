c5dea20f5e0c3fd57f80e5b9694c3eadf8062021
InluxBDX <talles.silva@gmail.com>
2020-08-04 23:30:39 -0300

    Ajuste mysql conexão

--- src/conecta.php
+++ src/conecta.php
@@ -3,7 +3,7 @@
 
 function is_localhost() {		
     // set the array for testing the local environment
-    $whitelist = array( '127.0.0.1', '::1' );
+    $whitelist = array( '127.0.0.1', '::1', '35.247.202.88' );
     
     // check if the server is in the array
     if ( in_array( $_SERVER['REMOTE_ADDR'], $whitelist ) ) {
