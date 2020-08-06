2ff84dd55e2db9984a1ee06fab9a94c92c4f35f2
InluxBDX <talles.silva@gmail.com>
2020-08-06 12:03:31 -0300

    Ajustes layour

--- src/conecta.php
+++ src/conecta.php
@@ -3,7 +3,7 @@
 
 function is_localhost() {		
     // set the array for testing the local environment
-    $whitelist = array( '127.0.0.1', '::1', '35.247.202.88' );
+    $whitelist = array( '127.0.0.1', '::1');
     
     // check if the server is in the array
     if ( in_array( $_SERVER['REMOTE_ADDR'], $whitelist ) ) {
@@ -15,13 +15,10 @@ function is_localhost() {
     
 }
 
-
-
 class conectaDB{
     
     static private $conexao;
-    
-   
+       
     static public function criarConexao(){
         $server = "localhost";
         $usuario ="root";
