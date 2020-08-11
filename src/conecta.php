d97f3474146bb365c82cb481b0a2947a810c5dc0
InluxBDX <talles.silva@gmail.com>
2020-08-11 19:30:52 -0300

    Erro MySQL -3

--- src/conecta.php
+++ src/conecta.php
@@ -3,7 +3,7 @@
 
 function is_localhost() {		
     // set the array for testing the local environment
-    $whitelist = array( '127.0.0.1', '::1', '35.247.202.88');
+    $whitelist = array( '127.0.0.1', '::1');
     
     // check if the server is in the array
     if ( in_array( $_SERVER['REMOTE_ADDR'], $whitelist ) ) {
@@ -21,19 +21,19 @@ class conectaDB{
        
     static public function criarConexao(){
        
+        $server = "localhost";
+        $usuario ="root";
+        $senha = "admin";
+        $db = "eweterapias";  
+         
         if(!is_localhost()){
             $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-            $server = $url["host"];
-            $usuario = $url["user"];
-            $senha = $url["pass"];
-            $db = substr($url["path"], 1);
-        }else{
-            $server = "localhost";
-            $usuario ="root";
-            $senha = "admin";
-            $db = "eweterapias";       
-            
-
+           if($url){
+                $server = $url["host"];
+                $usuario = $url["user"];
+                $senha = $url["pass"];
+                $db = substr($url["path"], 1);           
+             }
         }
         try{
             self:self::$conexao = mysqli_connect($server, $usuario, $senha, $db);
