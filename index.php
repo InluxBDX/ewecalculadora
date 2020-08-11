844f1854759d1a768616eae833e1bfe64f85a503
InluxBDX <talles.silva@gmail.com>
2020-08-11 20:10:07 -0300

    Ajuste final

--- index.php
+++ index.php
@@ -11,22 +11,6 @@
 
 <body>
 
-<?php
-
-echo $_SERVER['PHP_SELF'];
-echo "<br>";
-echo $_SERVER['SERVER_NAME'];
-echo "<br>";
-echo $_SERVER['HTTP_HOST'];
-echo "<br>";
-echo $_SERVER['HTTP_REFERER'];
-echo "<br>";
-echo $_SERVER['HTTP_USER_AGENT'];
-echo "<br>";
-echo $_SERVER['SCRIPT_NAME'];
-echo "<br>";
-echo $_SERVER['REMOTE_ADDR'];
-?>
 
 <div id="grid" class="grid-container">
 <div id="logo"> 
